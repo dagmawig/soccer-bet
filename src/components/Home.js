29c6a3e129fe5330227642f0a0beb8fcdd1fda4d
dagmawig <dgebreselasse@gmail.com>
2022-01-28 22:54:10 -0600

    added home page title and match buttons and styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,15 @@ function Home() {
     return (
         <div className='home container'>
             <div className='home_row row'>
-                   
+                <div className='home_header row'>
+                    <div className='home_header_title col-12'>
+                        WEEKEND MATCHES
+                    </div>
+                </div>
+                <div className='home_button-wrapper btn-group w-100 row' role='group' aria-label='match button'>
+                    <button type='button' className='sat_button btn-l col-6'>Saturday Matches</button>
+                    <button type='button' className='sun_button btn-l col-6'>Sunday Matches</button>
+                </div>
             </div>
         </div>
     )
