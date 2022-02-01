0c053342141d411b048d5a270af72d7a1f38d024
dagmawig <dgebreselasse@gmail.com>
2022-01-31 19:25:26 -0600

    added routers for history and home page.

--- src/App.js
+++ src/App.js
@@ -4,16 +4,33 @@ import Home from './components/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import History from './components/History';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
+
+  let homePage = <>
+    <Header />
+    <Home />
+    <Footer />
+  </>
+
+  let historyPage = <>
+    <Header />
+    <History />
+    <Footer />
+  </>
+
+
   return (
-    <>
-      <Header />
-      {/* <Home /> */}
-      <History/>
-      <Footer/>
-      
-    </>
+    <Router >
+      <div className='App'>
+        <Routes>
+          <Route path='/home' element={homePage} />
+          <Route path='/history' element={historyPage} />
+          <Route path="/" element={homePage} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,24 @@
 import React from "react";
 import './Footer.css';
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
         <div className="footer container">
             <div className="footer_row row">
                 <div className="footer_home col-6">
-                    <button className="footer_button">
-                        <i className="fa fa-home fa-2x"></i>
-                    </button>
+                    <Link to='/home'>
+                        <button className="footer_button">
+                            <i className="fa fa-home fa-2x"></i>
+                        </button>
+                    </Link>
                 </div>
                 <div className="footer_history col-6">
-                    <button className="footer_button">
-                        <i className="fa fa-history fa-2x"></i>
-                    </button>
+                    <Link to='/history'>
+                        <button className="footer_button">
+                            <i className="fa fa-history fa-2x"></i>
+                        </button>
+                    </Link>
                 </div>
             </div>
         </div>
